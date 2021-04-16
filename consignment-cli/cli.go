03cd81c0d4d563c609e9dbbaed695f5fc8704887
KapitanD <ddden97@gmail.com>
2021-04-16 23:41:08 +0300

    late first commit

---
+++ consignment-cli/cli.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	pbf "github.com/KapitanD/shipping-service/consignment"
+
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+const (
+	address         = "localhost:50051"
+	defaultFilename = "consignment.json"
+)
+
+// function parse gotten file
+func parseFile(file string) (*pbf.Consignment, error) {
+	var consignment *pbf.Consignment
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(data, &consignment)
+	return consignment, nil
+}
+
+func main() {
+	// Estable connect to server
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Can not connect: %v", err)
+	}
+	defer conn.Close()
+	client := pbf.NewShippingServiceClient(conn)
+
+	// Pass consignment.json in processing,
+	// else get path to file from commad line args
+	file := defaultFilename
+	if len(os.Args) > 1 {
+		file = os.Args[1]
+	}
+
+	consignment, err := parseFile(file)
+	if err != nil {
+		log.Fatalf("Impossible to parse file: %v", err)
+	}
+
+	r, err := client.CreateConsignment(context.Background(), consignment)
+	if err != nil {
+		log.Fatalf("Can not create: %v", err)
+	}
+
+	log.Printf("Created: %t", r.Created)
+}
---
+++ shippy-service-consignment/go.mod
@@ -0,0 +1,3 @@
+module github.com/KapitanD/shipping-service/shippy-service-consignment
+
+go 1.16
